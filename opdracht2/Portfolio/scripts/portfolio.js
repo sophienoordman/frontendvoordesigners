e5b36eff00f630d5bb2fc5e7b1ac5c3688d354df
sophienoordman <sophienxp@gmail.xom>
2019-03-13 12:17:03 +0100

    show-up functie werkt op meerdere onderdelen

--- opdracht2/Portfolio/scripts/portfolio.js
+++ opdracht2/Portfolio/scripts/portfolio.js
@@ -3,19 +3,22 @@
 /*eslint 'no-console':0*/
 
 
-function scrollApear (){
-    var introText = document.querySelector('.intro-text');
-    var introPosition = introText.getBoundingClientRect().top;
-    //vanaf boven scherm
-    //var screenPosition = window.innerHeight;
+function scrollApear() {
+    var showUp = document.querySelectorAll('.show-up');
+    //console.log(introText);
+    for (var i = 0; i < showUp.length ; i++) {
+        var introPosition = showUp[i].getBoundingClientRect().top;
+        //vanaf boven scherm
+        //var screenPosition = window.innerHeight;
 
-    //vanaf helescherm delen door 1.3
-    var screenPosition = window.innerHeight/ 1.2;
+        //vanaf helescherm delen door 1.3
+        var screenPosition = window.innerHeight / 1.2;
+        console.log(screenPosition);
 
-    if(introPosition< screenPosition){
-        introText.classList.add('intro-appear');
+        if (introPosition < screenPosition) {
+            showUp[i].classList.add('intro-appear');
+        }
     }
 }
 
 window.addEventListener('scroll', scrollApear);
-
